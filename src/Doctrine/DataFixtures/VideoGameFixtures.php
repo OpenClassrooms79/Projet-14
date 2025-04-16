bbb9504b7d1d7550762c2eaa0c6d61f0d5a2de00
Jocelyn <J0celyn@users.noreply.github.com>
2025-04-16 23:15:05 +0200

    nettoyage du code

--- src/Doctrine/DataFixtures/VideoGameFixtures.php
+++ src/Doctrine/DataFixtures/VideoGameFixtures.php
@@ -4,43 +4,19 @@ namespace App\Doctrine\DataFixtures;
 
 use App\Factory\TagFactory;
 use App\Factory\VideoGameFactory;
-use App\Model\Entity\User;
-use App\Model\Entity\VideoGame;
-use App\Rating\CalculateAverageRating;
-use App\Rating\CountRatingsPerValue;
-use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Generator;
 
-use function array_fill_callback;
-
 final class VideoGameFixtures extends Fixture implements DependentFixtureInterface
 {
     public function __construct(
         private readonly Generator $faker,
-        private readonly CalculateAverageRating $calculateAverageRating,
-        private readonly CountRatingsPerValue $countRatingsPerValue,
     ) {}
 
     public function load(ObjectManager $manager): void
     {
-        $users = $manager->getRepository(User::class)->findAll();
-
-        /*$videoGames = array_fill_callback(
-            0,
-            50,
-            fn(int $index): VideoGame
-                => (new VideoGame)
-                ->setTitle(sprintf('Jeu vidéo %d', $index))
-                ->setDescription($this->faker->paragraphs(10, true))
-                ->setReleaseDate(new DateTimeImmutable())
-                ->setTest($this->faker->paragraphs(6, true))
-                ->setRating(($index % 5) + 1)
-                ->setImageName(sprintf('video_game_%d.png', $index))
-                ->setImageSize(2_098_872),
-        );*/
         VideoGameFactory::createMany(50, function (int $index) {
             return [
                 'title' => sprintf('Jeu vidéo %d', $index),
@@ -54,13 +30,7 @@ final class VideoGameFixtures extends Fixture implements DependentFixtureInterfa
             ];
         });
 
-        // TODO : Ajouter les tags aux vidéos
-
-        //array_walk($videoGames, [$manager, 'persist']);
-
         $manager->flush();
-        // TODO : Ajouter des reviews aux vidéos
-
     }
 
     public function getDependencies(): array
