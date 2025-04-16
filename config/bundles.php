5eb2e11ea091af924d543f2c13c5b641d8c0830b
Jocelyn <J0celyn@users.noreply.github.com>
2025-04-16 22:23:17 +0200

    mise à jour des dépendances

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
