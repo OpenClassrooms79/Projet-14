8a95c0540fffa24103839eff48891b6cb9456b3d
Jocelyn <J0celyn@users.noreply.github.com>
2025-04-24 11:46:53 +0200

    test config ci.yml

--- tests/VideoGameRatingTest.php
+++ tests/VideoGameRatingTest.php
@@ -81,7 +81,7 @@ class VideoGameRatingTest extends WebTestCase
     public function testRatingFormNotDisplayedIfNotLoggedIn(): void
     {
         $client = static::createClient();
-        $client->request('GET', 'https://127.0.0.1:8000/jeu-video-7');
+        $client->request('GET', '/jeu-video-7');
         self::assertSelectorNotExists('FORM[name=review]');
     }
 
